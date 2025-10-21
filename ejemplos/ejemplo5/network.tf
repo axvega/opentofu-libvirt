db00f3bc2545ba1180bd2f6bd3bd553a5ad3ff0e
axvega <axvega420@gmail.com>
2025-10-21 14:21:22 +0200

    Ejemplo5end

--- ejemplos/ejemplo5/network.tf
+++ ejemplos/ejemplo5/network.tf
@@ -11,7 +11,7 @@ resource "libvirt_network" "nat-dhcp" {
   mode      = "nat"
   domain    = "example.com"
   addresses = ["192.168.100.0/24"]
-  bridge    = "virbr10"
+  bridge    = "virbr24"
   dhcp { enabled = true }
   dns { enabled = true }
   autostart = true
@@ -66,7 +66,7 @@ resource "libvirt_network" "nat-dhcp" {
 resource "libvirt_network" "muy-aislada" {
   name      = "muy-aislada"
   mode      = "none" # sin conectividad
-  bridge    = "virbr14"
+  bridge    = "virbr25"
   autostart = true
 }
 
